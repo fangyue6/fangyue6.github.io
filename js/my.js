453255896458886cfa893248de3a6fadb80e6c1d
fangyue <yue.fang.ser@gmail.com>
2017-09-22 14:30:07 +0800

    修正pdf加载错误问题

---
+++ js/my.js
@@ -0,0 +1,15 @@
+function showPdf(isShow) {
+            var state = "";
+            if (isShow) {
+                state = "block";
+            } else {
+                state = "none";
+            }
+            var pop = document.getElementById("pop");
+            pop.style.display = state;
+            var lightbox = document.getElementById("lightbox");
+            lightbox.style.display = state;
+        }
+function close() {
+    showPdf(false);
+}
